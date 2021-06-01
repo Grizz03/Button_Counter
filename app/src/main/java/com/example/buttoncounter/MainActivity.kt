f636a307a16796b95dcd2c7647e38de06716f43c
grizz03 <sawilson1110@yahoo.com>
2021-06-01 00:32:55 -0700

    declaring variables of widgets

--- app/src/main/java/com/example/buttoncounter/MainActivity.kt
+++ app/src/main/java/com/example/buttoncounter/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.buttoncounter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    // ID of the widget: type of widget -> then import
+    private var editText: EditText? = null
+    private var button: Button? = null
+    private var textView: TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.note_layout)  // Changed activity_main to note_layout for emulator preview
+        setContentView(R.layout.button_counter)
     }
 }
